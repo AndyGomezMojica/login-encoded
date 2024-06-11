92fe862cde02c050d4fd7427d6164b22ba7eb26b
andy <andygomezmojica@gmail.com>
2024-06-11 17:56:13 -0600

    add username column to structure the email

--- src/main/java/com/andygomez/register_login/flow/model/UserModel.java
+++ src/main/java/com/andygomez/register_login/flow/model/UserModel.java
@@ -19,6 +19,8 @@ public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "user_name")
+    private String userName;
     private String name;
     @Column(name = "last_name")
     private String lastName;
